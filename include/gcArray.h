3ca38f5c7b997289cd2b79caa3702b2999b8d06a
Daniel Hall <danne.01@hotmail.com>
2025-05-30 19:57:33 +0200

    chore: Renamed library to WECL, renamed all files

--- include/gcArray.h
+++
@@ -1,8 +0,0 @@
-#ifndef _INCLUDE_GCARRAY_H_
-#define _INCLUDE_GCARRAY_H_
-
-// #define gc_countof(arr)
-#define gc_lengthof(arr) sizeof(arr) / sizeof(*arr)
-
-
-#endif /* _INCLUDE_GCARRAY_H_ */
--- include/gcTimer.h
+++
@@ -1,5 +0,0 @@
-#ifndef _GC_REALTIME_TIMER_H_
-#define _GC_REALTIME_TIMER_H_
-
-
-#endif /* _GC_REALTIME_TIMER_H_ */
