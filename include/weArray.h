bb28c223e1385c3495098607d10bba33915762c7
Daniel Hall <danne.01@hotmail.com>
2025-05-30 20:10:50 +0200

    fix: naming convention

--- include/weArray.h
+++ include/weArray.h
@@ -2,7 +2,7 @@
 #define _WECL_ARRAY_H_
 
 // #define gc_countof(arr)
-#define we_lengthof(arr) sizeof(arr) / sizeof(*arr)
+#define wecl_lengthof(arr) sizeof(arr) / sizeof(*arr)
 
 
 #endif /* _WECL_ARRAY_H_ */
