501f0fb0b04d0ea4e000ab55138545e68bf18918
Daniel Hall <danne.01@hotmail.com>
2025-07-06 23:20:14 +0200

    feat!: Remade raii to be used as an attribute instead of a macro definition

--- include/weMemory.h
+++ include/weMemory.h
@@ -10,29 +10,12 @@ void* weDefaultCalloc(size_t size);
 
 #if !__has_attribute(cleanup)
 #error "The `cleanup` attribute is not supported with current compiler."
+#else
+
+
+#define wecl_raii __attribute__((cleanup(weDefaultFree)))
+#define wecl_raiiA(...) __attribute__((cleanup(__VA_ARGS__)))
+
+
 #endif  // __has_attribute(cleanup)
-
-
-// Decalare variable which has a destructor called at the end of the scope
-#define wecl_raii(type, name, destructor) type __attribute__((cleanup(destructor))) name
-
-#define wecl_araii(name, destructor) auto __attribute__((cleanup(destructor))) name
-
-// Decalre and allocate memory. Memory is freed at the end of the scope
-#define wecl_ralloc(type, name, amount, alloc, free)                                               \
-    type __attribute__((cleanup(free))) name = alloc(sizeof(*name) * amount)
-
-
-// Declare and allocate memory from the weDefaultAlloc function, memory is automatically freed at
-// the end of the scope
-#define wecl_rmalloc(type, name, amount)                                                           \
-    type __attribute__((cleanup(weDefaultFree))) name = weDefaultAlloc(sizeof(*name) * amount)
-
-
-// Declare and allocate memory from the weDefaultCalloc function, memory is automatically freed at
-// the end of the scope
-#define wecl_rcalloc(type, name, amount)                                                           \
-    type __attribute__((cleanup(weDefaultFree))) name = weDefaultCalloc(sizeof(*name) * amount)
-
-
-#endif /* _INCLUDE_WEMEMORY_H_ */
+#endif  /* _INCLUDE_WEMEMORY_H_ */
